738c629f09e4b0f8df979d92ab6fac0ae9f7f17a
sergio <sergio@Roxana-PC>
2018-06-15 16:15:43 -0300

    efecto sogen y efecto Olla ahora si

---
+++ src/modelo/EfectoOllaDeLaCodicia.java
@@ -0,0 +1,17 @@
+package modelo;
+
+public class EfectoOllaDeLaCodicia implements Efecto {
+	
+	private Jugador jugador; 
+	
+	public EfectoOllaDeLaCodicia(Jugador unJugador) {
+		jugador = unJugador;
+	}
+
+	@Override
+	public void activarse() {
+		jugador.tomarCartaDelMazo();
+		jugador.tomarCartaDelMazo();
+	}
+
+}
