a1d601b2d083d54cceb40feb2c56a4186e1c4f31
sberoch <sberoch@gmail.com>
2018-06-22 20:10:12 -0300

    main basico con nuevo paquete vista

---
+++ src/vista/Main.java
@@ -0,0 +1,21 @@
+package vista;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+	public static void main(String[] args) {
+
+		launch(args);
+	}
+	
+	
+	 public void start(Stage stage) throws Exception {
+		
+		stage.setTitle("AL-GO-OH");
+	 	stage.show();
+	 }
+
+
+}
